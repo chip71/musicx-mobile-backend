9d956cb49a93547769c2351a00b8026b38f10f44
chip71 <vuquang07012004@gmail.com>
2025-11-05 14:06:21 +0700

    Fix dotenv path and update environment config for Render

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js (main backend entry)
-require('dotenv').config({ path: __dirname + '/../.env' });
+// require('dotenv').config({ path: __dirname + '/../.env' });
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
